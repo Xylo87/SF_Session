8b7e0e8bbdfdc557435e409a45f43f8a25222997
Xylo87 <theo.arbogast@hotmail.fr>
2025-02-09 14:00:32 +0100

    feat/update : ajout de photo de profils fonctionnel

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -11,6 +11,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 final class StagiaireController extends AbstractController
 {
@@ -28,7 +32,12 @@ final class StagiaireController extends AbstractController
     #[Route('/stagiaire/new', name: 'new_stagiaire')]
     #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function new_edit(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
+    public function new_edit(Stagiaire $stagiaire = null, 
+    Request $request, 
+    EntityManagerInterface $entityManager, 
+    SluggerInterface $slugger,
+    #[Autowire('%kernel.project_dir%/public/uploads/photos')] string $photosDirectory
+    ): Response
     {
         if (!$stagiaire) {
             $stagiaire = new Stagiaire();
@@ -40,6 +49,23 @@ final class StagiaireController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             
             $stagiaire = $form->getData();
+            $photoFile = $form->get('photo')->getData();
+
+            if ($photoFile) {
+                $originalFileName = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFileName = $slugger->slug($originalFileName);
+                $newFileName = $safeFileName.'-'.uniqid().'.'.$photoFile->guessExtension();
+
+                try {
+                    $photoFile->move($photosDirectory, $newFileName);
+                } catch (FileException $e) {
+                    ('Erreur lors du chargement du fichier : '.$e->getMessage()); die;
+                }
+            } else {
+                $newFileName = null;
+            }
+
+            $stagiaire->setPhoto($newFileName);
 
             $entityManager->persist($stagiaire);
             $entityManager->flush();
